f08fda87bba2aa1597f0ea268b2444746945f973
Liu <liumin2011@gmail.com>
2018-09-17 09:35:42 +0800

    Initial Commit

---
+++ JavaScriptBridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavaScriptBridge
+//
+//  Created by Liu on 2018/9/17.
+//  Copyright © 2018年 mzying.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JavaScriptBridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavaScriptBridge
+//
+//  Created by Liu on 2018/9/17.
+//  Copyright © 2018年 mzying.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
